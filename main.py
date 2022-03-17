eea928b58b18dc51a8feb7f7ad5ae7904c7911b3
Raphael Sparenberg <raphaelsparenberg@kateinnovations.com>
2022-03-17 17:42:00 +0100

    better format and typo's

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 import re
 import sys
 
-#Checking if the filename exists otherwise exit
-#Possible improvement: Ask user again for filename and dont exit right away
+# Checking if the filename exists otherwise exit
+# Possible improvement: Ask user again for filename and don't exit right away
 try:
     text_file = open(input('Enter text file name. Example: test.txt: '), 'r')
 except FileNotFoundError as e:
     print('File not found, check filename.')
     sys.exit()
 
-#Reading the text file
+# Reading the text file
 file_text = text_file.read()
 text_file.close()
 
-#Iniatiate the search
+# Initiate the search
 keys_found = re.findall(r"\bvaluation_\w+", file_text)
 
-#Removing possible dublicates from array 
+# Removing possible duplicates from array
 keys_without_duplicates = list(dict.fromkeys(keys_found))
 
-#Getting the amount of keys found
+# Getting the amount of keys finds
 keys_amount_found = len(keys_without_duplicates)
 
-#Checking if keys are found
+# Checking if keys are found
 if keys_amount_found > 0:
     print(f"Found {str(keys_amount_found)} keys in file!")
     single_or_array = input('You want them printed out as array or single? single/array: ')
+    single_or_array = single_or_array.strip()
     if single_or_array == 'array':
         print('\n', keys_without_duplicates)
     elif single_or_array == 'single':
@@ -35,5 +36,4 @@ if keys_amount_found > 0:
         print('Input nog recognized.')
         sys.exit()
 else:
-    print('No keys found! Check if file is saved as .txt file. You can do this easely with Notepad ++')
-
+    print('No keys found! Check if file is saved as .txt file. You can do this easily with Notepad ++')
